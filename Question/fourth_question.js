2f9668277a1d5bead1fcfbabccba1b7c021c69ce
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-03-07 20:58:34 +0530

    fourth_question_added

---
+++ Question/fourth_question.js
@@ -0,0 +1,20 @@
+// Function to reverse the array in-place
+function reverseString(s) {
+
+    let left = 0;                // start pointer
+    let right = s.length - 1;   // end pointer
+
+    // continue until pointers meet
+    while (left < right) {
+
+        // swap characters
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        // move pointers toward center
+        left++;
+        right--;
+    }
+
+}
