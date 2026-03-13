a1c5fa55c2bd168f2ff40712d38a7749b11bdc1d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-03-13 20:21:57 +0530

    seventh_question added

---
+++ Question/seventh_question.js
@@ -0,0 +1,24 @@
+// Function to count palindromic substrings
+function countSubstrings(s) {
+
+    let count = 0;
+
+    // check palindrome by expanding from center
+    function expand(left, right) {
+
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            count++;      // found a palindrome
+            left--;       // expand left
+            right++;      // expand right
+        }
+    }
+
+    // check every index as center
+    for (let i = 0; i < s.length; i++) {
+
+        expand(i, i);       // odd length palindrome
+        expand(i, i + 1);   // even length palindrome
+    }
+
+    return count;
+}
