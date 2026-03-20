6b15739d5343c8a4e6649307c728722e619f0de9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-03-20 19:24:29 +0530

    ninth question added

---
+++ Question/ninth_question.js
@@ -0,0 +1,59 @@
+// Main function
+function searchRange(nums, target) {
+
+    return [findFirst(nums, target), findLast(nums, target)];
+}
+
+
+// Find first occurrence of target
+function findFirst(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            result = mid;     // store answer
+            right = mid - 1;  // move left to find earlier index
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
+
+
+// Find last occurrence of target
+function findLast(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            result = mid;     // store answer
+            left = mid + 1;   // move right to find later index
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
