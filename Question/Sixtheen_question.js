fa55748a78ee2a8feb7a02726105e9bf01a81c82
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-02 08:04:47 +0530

    sixtheen code added Done

---
+++ Question/Sixtheen_question.js
@@ -0,0 +1,40 @@
+// Definition of ListNode
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+function reverseKGroup(head, k) {
+
+    let curr = head;
+    let count = 0;
+
+    // Step 1: Check if at least k nodes exist
+    while (curr && count < k) {
+        curr = curr.next;
+        count++;
+    }
+
+    // If we have k nodes → reverse
+    if (count === k) {
+
+        // Step 2: Reverse first k nodes
+        let prev = null;
+        let curr = head;
+
+        for (let i = 0; i < k; i++) {
+            let next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Step 3: Recursively process remaining list
+        head.next = reverseKGroup(curr, k);
+
+        return prev; // new head after reverse
+    }
+
+    // If less than k nodes → return as it is
+    return head;
+}
