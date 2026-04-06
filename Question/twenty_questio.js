66bfe809c82fe388a6a01c5b66c6961d90aaafa9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-06 17:25:27 +0530

    solution added of twenty question Done

---
+++ Question/twenty_questio.js
@@ -0,0 +1,34 @@
+var pairSum = function(head) {
+    // Step 1: Find middle
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Step 2: Reverse second half
+    let prev = null;
+    let curr = slow;
+
+    while (curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    // Step 3: Compare twin sums
+    let first = head;
+    let second = prev;
+    let maxSum = 0;
+
+    while (second) {
+        maxSum = Math.max(maxSum, first.val + second.val);
+        first = first.next;
+        second = second.next;
+    }
+
+    return maxSum;
+};
