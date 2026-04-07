4a7ba65b81e859cf6f9484886adc1ebf8b79e214
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-07 20:33:09 +0530

    code added in twenty-one done

--- Question/twenty-one_question.js
+++ Question/twenty-one_question.js
@@ -1,1 +1,21 @@
-// 162 question on leetcode
+// 162 question on leetcode 
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Compare mid with next element
+        if (nums[mid] < nums[mid + 1]) {
+            // Peak is on the right
+            left = mid + 1;
+        } else {
+            // Peak is on the left (including mid)
+            right = mid;
+        }
+    }
+
+    // left === right → peak index
+    return left;
+};
