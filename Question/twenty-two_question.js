11808fab74833254aa01a994981b9c5588dd44fe
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-08 12:31:47 +0530

    solution added in twenty-two question

---
+++ Question/twenty-two_question.js
@@ -0,0 +1,22 @@
+// 3 question on leetcode
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        // If duplicate found, shrink window
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        // Add current character
+        set.add(s[right]);
+
+        // Update max length
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
