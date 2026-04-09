160e84960444188315d323f4eb29d4ea7c7c163c
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-09 12:19:08 +0530

    answer is added in twenty-three question Done

---
+++ Question/twenty-three_question.js
@@ -0,0 +1,32 @@
+// 2 question on leetcode
+
+// Definition for singly-linked list
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode(0);   // dummy head
+    let current = dummy;           // pointer for result list
+    let carry = 0;                 // carry value
+
+    // loop until both lists and carry are exhausted
+    while (l1 || l2 || carry) {
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
+
+        let sum = val1 + val2 + carry;
+
+        carry = Math.floor(sum / 10); // update carry
+        let digit = sum % 10;         // current digit
+
+        current.next = new ListNode(digit); // create node
+        current = current.next;             // move forward
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return dummy.next; // return result list
+};
