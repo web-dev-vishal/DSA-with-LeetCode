0970d58f79a847e6ccd389e11136c9346c50cd78
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-04-18 20:56:51 +0530

    update in codebase Done

--- Question/thirty-one_question.js
+++ Question/thirty-one_question.js
@@ -23,27 +23,27 @@ var search = function(nums, target) {
     return -1; // target not found
 };
 /*
-input : nums = [1, 3, 5, 7, 9]
-target = 5
-output : 3
-Explanation:
-The array is sorted. The middle element becomes 5, which matches the target, so the function correctly returns index 2.
+1. input : nums = [1, 3, 5, 7, 9]
+   target = 5
+   output : 3
+   Explanation: 
+   The array is sorted. The middle element becomes 5, which matches the target, so the function correctly returns index 2.
 
-input : nums = [2, 4, 6, 8, 10, 12]
-target = 10
-output : 4
-Explanation:
-Binary search narrows down the range and finds 10 at index 4.
+2. input : nums = [2, 4, 6, 8, 10, 12]
+   target = 10
+   output : 4
+   Explanation:
+   Binary search narrows down the range and finds 10 at index 4.
 
-input: nums = [10, 5, 2, 8, 3]
-target = 8
-output : -1   // (Incorrect result)
-Explanation:
-Your algorithm assumes the array is sorted in ascending order, but this input array is unsorted.
+3. input: nums = [10, 5, 2, 8, 3]
+   target = 8
+   output : -1   // (Incorrect result)
+   Explanation:
+   Your algorithm assumes the array is sorted in ascending order, but this input array is unsorted.
 
-Binary search only works correctly on sorted arrays. Since the order is random here, the logic:
+   Binary search only works correctly on sorted arrays. Since the order is random here, the logic:
 
-if (nums[mid] < target)
+   if (nums[mid] < target)
 
-leads the search in the wrong direction, causing it to miss the target even though 8 exists in the array.
+   leads the search in the wrong direction, causing it to miss the target even though 8 exists in the array.
 */
